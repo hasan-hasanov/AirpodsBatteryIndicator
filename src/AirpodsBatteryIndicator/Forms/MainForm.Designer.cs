6bd3a2e2b1188a475a4b664d38da56465774d8e1
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-04-07 17:55:04 +0300

    Add image assets

--- src/AirpodsBatteryIndicator/Forms/MainForm.Designer.cs
+++ src/AirpodsBatteryIndicator/Forms/MainForm.Designer.cs
@@ -35,7 +35,7 @@ namespace AirpodsBatteryIndicator
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(126, 65);
+            this.lblStatus.Location = new System.Drawing.Point(144, 59);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(38, 15);
             this.lblStatus.TabIndex = 0;
@@ -45,8 +45,12 @@ namespace AirpodsBatteryIndicator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(350, 200);
             this.Controls.Add(this.lblStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainForm";
             this.Text = "Airpods Battery Status";
             this.ResumeLayout(false);
