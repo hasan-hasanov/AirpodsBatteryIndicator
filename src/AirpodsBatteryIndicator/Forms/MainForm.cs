16f36e688abb8d65a57ccfa4f40d7f4b4f09cdda
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-04-14 21:54:00 +0300

    Remove redundant method

--- src/AirpodsBatteryIndicator/Forms/MainForm.cs
+++ src/AirpodsBatteryIndicator/Forms/MainForm.cs
@@ -152,12 +152,6 @@ namespace AirpodsBatteryIndicator
             }
         }
 
-        private bool IsFlipped(char[] hex)
-        {
-            string isFlippedInfoBinary = Convert.ToString(short.Parse(hex[10].ToString()) + 0x10, 2);
-            return isFlippedInfoBinary[3] == '0';
-        }
-
         private void SetTryIconRegardingBattery(AirpodBleParser airpods)
         {
             if (airpods.IsConnected)
