13a27176a5c6952156a516032065115f4dc6b876
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-04-16 17:45:10 +0300

    Add empty wpf project

---
+++ src/ABI.UI/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace ABI.UI
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ src/ABI.UI/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ABI.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
