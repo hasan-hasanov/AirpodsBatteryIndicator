4298fa917e4d348058faaf6d934063fb3499add3
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-04-17 18:05:20 +0300

    Add primitive command handler

---
+++ src/ABI.ViewModel/Commands/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ABI.ViewModel.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
--- src/ABI.ViewModel/ViewModels/MainViewModel.cs
+++ src/ABI.ViewModel/ViewModels/MainViewModel.cs
@@ -1,12 +1,40 @@
-﻿namespace ABI.ViewModel.ViewModels
+﻿using ABI.ViewModel.Commands;
+using System.Windows.Input;
+
+namespace ABI.ViewModel.ViewModels
 {
     public class MainViewModel
     {
         public MainViewModel()
         {
             LeftEarbudBattery = "90";
+
+            OpenClickCommand = new RelayCommand(e => OpenClick(), p => true);
+            SettingsClickCommand = new RelayCommand(e => SettingsClick(), p => true);
+            ExitClickCommand = new RelayCommand(e => ExitClick(), p => true);
         }
 
         public string LeftEarbudBattery { get; set; }
+
+        public ICommand OpenClickCommand { get; set; }
+
+        public ICommand SettingsClickCommand { get; set; }
+
+        public ICommand ExitClickCommand { get; set; }
+
+        public void OpenClick()
+        {
+
+        }
+
+        public void SettingsClick()
+        {
+
+        }
+
+        public void ExitClick()
+        {
+
+        }
     }
 }
