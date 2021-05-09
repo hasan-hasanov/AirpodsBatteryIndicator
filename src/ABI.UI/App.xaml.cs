e8fb67189604f86b400d273c9d282275e211cf29
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-05-09 17:56:39 +0300

    Add temporary debugging to observe errors on real use case.

--- src/ABI.UI/App.xaml.cs
+++ src/ABI.UI/App.xaml.cs
@@ -1,12 +1,27 @@
 ﻿using ABI.ViewModel.AirpodsBle;
 using ABI.ViewModel.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 using System.Windows;
 
 namespace ABI.UI
 {
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+#warning Remove this before release!!!
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            string path = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/AirpodsErrors.txt";
+            File.AppendAllLines(path, new string[] { e.ExceptionObject.ToString(), Environment.NewLine });
+        }
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             IServiceCollection services = new ServiceCollection();
