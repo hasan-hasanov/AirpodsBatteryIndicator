58fe2b3d8e5246424223894f557604f869615bc4
hasan <hasan@PRIME>
2022-11-19 01:55:00 +0200

    Initial WinUI 3 project

---
+++ src/UI/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+using Microsoft.UI.Xaml;
+
+namespace UI
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }
+    }
+}
